740e09c38ebd7f3ee0f0748a8b0b29782d4078e8
nnstanusic <ns45766@gmail.com>
2022-10-03 23:44:47 +0200

    Update Testaton.cmake

--- Testaton.cmake
+++ Testaton.cmake
@@ -20,7 +20,8 @@ FOREACH(subdir ${SUBDIRS}) # Iterate over subdirectories and create tests for ea
     string(REGEX REPLACE ".*/(.*).cpp" "\\1" target_name ${test})
 
     add_executable(${target_name} ${test})
-    target_link_libraries(${target_name} PRIVATE ${test_deps})
+    target_link_libraries(${target_name} PRIVATE ${testaton_deps})
     add_test(NAME "${subdir};${target_name}" COMMAND ${target_name})
+    message(STATUS "Adding test: ${test}")
   ENDFOREACH()
 ENDFOREACH()
