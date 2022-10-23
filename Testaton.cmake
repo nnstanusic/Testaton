373b39ae8122ba1fd256189bfdd75df6ecbef06c
Nino Nikola Stanusic <ns45766@gmail.com>
2022-10-23 17:39:33 +0200

    Update Testaton.cmake

--- Testaton.cmake
+++ Testaton.cmake
@@ -13,10 +13,10 @@ ENDMACRO()
 
 SUBDIRLIST(SUBDIRS ${CMAKE_CURRENT_SOURCE_DIR})
 
+set(_TESTATON_TEST_LIST)
+
 FOREACH(subdir ${SUBDIRS}) # Iterate over subdirectories and create tests for each .cpp
   file(GLOB_RECURSE tests "${subdir}/*.cpp")
-
-  set(_TESTATON_TEST_LIST)
 
   FOREACH(test ${tests})
     string(REGEX REPLACE ".*/(.*).cpp" "\\1" target_name ${test})
@@ -25,14 +25,14 @@ FOREACH(subdir ${SUBDIRS}) # Iterate over subdirectories and create tests for ea
     # add_executable("${target}" ${test})
     list(APPEND _TESTATON_TEST_LIST ${test})
   ENDFOREACH()
-
-  create_test_sourcelist(Tests unit_tests.cpp ${_TESTATON_TEST_LIST})
-
-  add_executable(unit_tests ${Tests})
-  target_link_libraries(unit_tests PRIVATE ${testaton_deps})
-
-  foreach(test ${TestsToRun})
-    get_filename_component(TName ${test} NAME_WE)
-    add_test(NAME ${TName} COMMAND unit_tests ${TName})
-  endforeach()
 ENDFOREACH()
+
+create_test_sourcelist(Test_sources unit_tests.cpp ${_TESTATON_TEST_LIST})
+add_executable(unit_tests ${Test_sources})
+
+target_link_libraries(unit_tests PRIVATE ${testaton_deps})
+
+foreach(test ${_TESTATON_TEST_LIST})
+  get_filename_component(TName ${test} NAME_WE)
+  add_test(NAME ${TName} COMMAND unit_tests ${TName})
+endforeach()
